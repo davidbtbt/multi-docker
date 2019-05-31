e0778d33032d4246a254705c9f72d24f0e13eaba
davidbtbt <heroku.com@qualitybrain.com>
2019-05-31 16:23:59 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+	return (
+		<Router>
+			<div className='App'>
+				<header className='App-header'>
+					<h1> Welcome to React</h1>
+					<Link to='/'>Home</Link>
+					<Link to='/otherpage'>Other Page</Link>
+				</header>
+
+				<Route exact path='/' component={Fib} />
+				<Route path='/otherpage' component={OtherPage} />
+			</div>
+		</Router>
+	);
+}
+
+export default App;
